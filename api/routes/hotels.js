2c48e746d8945d4c34750cb36e19f7299b6c3b46
Anchal Pawar <anchalpawar62@gmail.com>
2024-03-04 12:15:38 +0530

    My First Full Stack project

---
+++ api/routes/hotels.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createHotel, deleteHotel, getAllHotel, getHotel, updateHotel, countByCity, countByType } from "../controllers/hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//create
+router.post("/", verifyAdmin, createHotel);
+
+//update
+router.put("/:Id", verifyAdmin, updateHotel);
+
+//delete
+router.delete("/:Id", verifyAdmin, deleteHotel);
+
+//get
+router.get("/find/:Id", getHotel);
+
+//get all
+router.get("/", getAllHotel);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+
+export default router
